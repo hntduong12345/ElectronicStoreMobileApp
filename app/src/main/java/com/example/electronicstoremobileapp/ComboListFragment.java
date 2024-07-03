e236f1221520980471d4a4ba85f2e62540aeb5b8
hntduong12345 <hntduong2003@gmail.com>
2024-07-03 21:19:56 +0700

    Bring all created ui from test ui to Backend-ThaiDuong

--- app/src/main/java/com/example/electronicstoremobileapp/ComboListFragment.java
+++ app/src/main/java/com/example/electronicstoremobileapp/ComboListFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link OrderFragment#newInstance} factory method to
+ * Use the {@link ComboListFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class OrderFragment extends Fragment {
+public class ComboListFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class OrderFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public OrderFragment() {
+    public ComboListFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class OrderFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment OrderFragment.
+     * @return A new instance of fragment ComboListFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static OrderFragment newInstance(String param1, String param2) {
-        OrderFragment fragment = new OrderFragment();
+    public static ComboListFragment newInstance(String param1, String param2) {
+        ComboListFragment fragment = new ComboListFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class OrderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order, container, false);
+        return inflater.inflate(R.layout.fragment_combo_list, container, false);
     }
 }
--- app/src/main/java/com/example/electronicstoremobileapp/ShopPageFragment.java
+++ app/src/main/java/com/example/electronicstoremobileapp/ShopPageFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link CustomerProfileFragment#newInstance} factory method to
+ * Use the {@link ShopPageFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CustomerProfileFragment extends Fragment {
+public class ShopPageFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class CustomerProfileFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public CustomerProfileFragment() {
+    public ShopPageFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class CustomerProfileFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment CustomerProfileFragment.
+     * @return A new instance of fragment ShopPageFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static CustomerProfileFragment newInstance(String param1, String param2) {
-        CustomerProfileFragment fragment = new CustomerProfileFragment();
+    public static ShopPageFragment newInstance(String param1, String param2) {
+        ShopPageFragment fragment = new ShopPageFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class CustomerProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_customer_profile, container, false);
+        return inflater.inflate(R.layout.fragment_shop_page, container, false);
     }
 }
