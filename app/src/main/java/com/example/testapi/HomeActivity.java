71bc64cb355a28bee72fe2f32ec10f1c3040bf58
hntduong12345 <hntduong2003@gmail.com>
2024-07-03 16:01:12 +0700

    Temp layout design

---
+++ app/src/main/java/com/example/testapi/HomeActivity.java
@@ -0,0 +1,58 @@
+package com.example.testapi;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationViewHomeNav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, new HomePageFragment())
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit();
+                return true;
+            case R.id.shop:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, new ShopPageFragment())
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit();
+                return true;
+            case R.id.cart:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, new CartPageFragment())
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit();
+                return true;
+            case R.id.person:
+                getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, new UserPageFragment())
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit();
+                return true;
+        }
+        return false;
+    }
+}
