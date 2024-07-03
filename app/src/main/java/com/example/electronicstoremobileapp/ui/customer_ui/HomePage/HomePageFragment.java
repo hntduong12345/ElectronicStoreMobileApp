7a66833149cf820701a3feff9e3da40ef28a11ac
hntduong12345 <hntduong2003@gmail.com>
2024-07-03 21:53:54 +0700

    Setup Folder structure

--- app/src/main/java/com/example/electronicstoremobileapp/ui/customer_ui/HomePage/HomePageFragment.java
+++ app/src/main/java/com/example/electronicstoremobileapp/ui/customer_ui/HomePage/HomePageFragment.java
@@ -1,4 +1,4 @@
-package com.example.electronicstoremobileapp;
+package com.example.electronicstoremobileapp.ui.customer_ui.HomePage;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.electronicstoremobileapp.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link CartPageFragment#newInstance} factory method to
+ * Use the {@link HomePageFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CartPageFragment extends Fragment {
+public class HomePageFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class CartPageFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public CartPageFragment() {
+    public HomePageFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class CartPageFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment CartPageFragment.
+     * @return A new instance of fragment HomePageFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static CartPageFragment newInstance(String param1, String param2) {
-        CartPageFragment fragment = new CartPageFragment();
+    public static HomePageFragment newInstance(String param1, String param2) {
+        HomePageFragment fragment = new HomePageFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class CartPageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart_page, container, false);
+        return inflater.inflate(R.layout.fragment_home_page, container, false);
     }
 }
--- app/src/main/java/com/example/electronicstoremobileapp/ui/customer_ui/UserPage/SettingsPageFragment.java
+++ app/src/main/java/com/example/electronicstoremobileapp/ui/customer_ui/UserPage/SettingsPageFragment.java
@@ -1,4 +1,4 @@
-package com.example.electronicstoremobileapp;
+package com.example.electronicstoremobileapp.ui.customer_ui.UserPage;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.electronicstoremobileapp.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ComboDetailFragment#newInstance} factory method to
+ * Use the {@link SettingsPageFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ComboDetailFragment extends Fragment {
+public class SettingsPageFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class ComboDetailFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public ComboDetailFragment() {
+    public SettingsPageFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class ComboDetailFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment ComboDetailFragment.
+     * @return A new instance of fragment SettingsPageFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static ComboDetailFragment newInstance(String param1, String param2) {
-        ComboDetailFragment fragment = new ComboDetailFragment();
+    public static SettingsPageFragment newInstance(String param1, String param2) {
+        SettingsPageFragment fragment = new SettingsPageFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class ComboDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_combo_detail, container, false);
+        return inflater.inflate(R.layout.fragment_settings_page, container, false);
     }
 }
